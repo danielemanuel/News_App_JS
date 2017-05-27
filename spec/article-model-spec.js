79f4d2cf480c854f1b575eea69ae027c5fdd8213
danielemanuel <daniel.costea@vkernel.ro>
2017-05-27 13:14:47 +0100

    Article has a body

--- spec/article-model-spec.js
+++ spec/article-model-spec.js
@@ -1,7 +1,8 @@
 (function ArticleModelInitialize() {
-    var article = new Article('This is headline');
+    var article = new Article('This is headline','This is the article body');
     console.log("Test 1: Article inistialize ");
     assert.isTrue(article.returnHeadline() === 'This is headline');
-
+    console.log("Test 1: Article has a body ");
+    assert.isTrue(article.returnBody() === 'This is the article body');
 
 })();
--- src/article-model.js
+++ src/article-model.js
@@ -1,12 +1,17 @@
 (function(exports) {
-    function Article(headline) {
+
+    function Article(headline, body) {
       this.headline = headline;
+      this.body = body;
     }
 
     Article.prototype.returnHeadline =function() {
         return this.headline;
     };
 
+    Article.prototype.returnBody = function() {
+        return this.body;
+    };
 
 
     exports.Article = Article;
